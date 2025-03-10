3c3ca66e42ce2dfcf757826f4a8c9062108f6ae1
AnwarMohammadi2001 <anwarmohammadi1390@gmail.com>
2025-03-10 09:45:34 +0430

    add the files

---
+++ cafe-roya/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus ad
+      similique recusandae delectus ipsam eveniet corporis necessitatibus quam
+      cupiditate, accusantium obcaecati, impedit facere dolores asperiores
+      blanditiis culpa. Exercitationem, id vel.
+    </div>
+  );
+};
+
+export default App;
